f5d9c03bba14f155c7fa325485597fda14a5aff3
Toshi <dtkellogg10@gmail.com>
2023-01-05 18:58:26 -0300

    Refactoring - techniques and fizzbuzz cleaned up

--- JS/problems/easy/412fizzBuzz.js
+++ JS/problems/easy/412fizzBuzz.js
@@ -1,37 +1,37 @@
 n = 3  // Output: ["1","2","Fizz"]
-// n = 15  // Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+n = 15  // Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
 
 
 
 // 🔥
 var fizzBuzz = function(n) {
-    return new Array(n).fill(0).map((a, i) => (++i % 3 ? '' : 'Fizz') + (i % 5 ? '' : 'Buzz') || '' + i);
+  return new Array(n).fill(0).map((a, i) => (++i % 3 ? '' : 'Fizz') + (i % 5 ? '' : 'Buzz') || '' + i);
 };
 
 
 
 // WHY DOESNT BELOW CODE WORK
-var fizzBuzz = function(n) {
-    let arr = new Array(n).fill(0)
+// var fizzBuzz = function(n) {
+//     let arr = new Array(n).fill(0)
     
-    arr.map((a, i) => (++i % 3 ? '' : 'Fizz') + (i % 5 ? '' : 'Buzz') || '' + i)
-    console.log(arr)
-    return arr
-};
+//     arr.map((a, i) => (++i % 3 ? '' : 'Fizz') + (i % 5 ? '' : 'Buzz') || '' + i)
+//     console.log(arr)
+//     return arr
+// };
 
 
 ////////////////////////////////////////////////////////////////
 // same idea as 1-liner
-// var fizzBuzz = function(n) {
-//     let res = [];
-//     for(let i = 1; i <= n; i++) {
-//         if(i % 3 === 0 && i % 5 === 0) res.push("FizzBuzz")
-//         else if(i % 3 === 0) res.push("Fizz")
-//         else if(i % 5 === 0 ) res.push("Buzz")
-//         else res.push(i.toString())
-//     }
-//     return res
-// };
+var fizzBuzz = function(n) {
+    let res = [];
+    for(let i = 1; i <= n; i++) {
+        if(i % 3 === 0 && i % 5 === 0) res.push("FizzBuzz")
+        else if(i % 3 === 0) res.push("Fizz")
+        else if(i % 5 === 0 ) res.push("Buzz")
+        else res.push(i.toString())
+    }
+    return res
+};
 
 
 ////////////////////////////////////////////////////////////////////////////////
