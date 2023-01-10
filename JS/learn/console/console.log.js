41d7c28f831957b4ed0a13235f156d5f37063af1
Toshi <dtkellogg10@gmail.com>
2023-01-10 19:18:39 -0300

    Refactoring - cleaning up console.log code

--- JS/learn/console/console.log.js
+++ JS/learn/console/console.log.js
@@ -2,34 +2,34 @@
 
 const person = {name: 'Toshi', age: '30', favFood: 'burrito', pets: ['boo', 'fred', 'poncho']}
 
-// #1 - All options
+// #1 - All options - //
 console.log(console)
 
-// #2 - General
+// #2 - General - //
 // console.log(person)
 
-// #3 - Drop down format
+// #3 - Drop down format - //
 // console.dir(person)
 
-// #4 - Table format
+// #4 - Table format - //
 // console.table(person)
 
-// #5 - Start a timer
+// #5 - Start a timer - //
 console.time()
-// #6 - Log time since timer started
+// #6 - Log time since timer started - //
 console.timeLog()
 console.timeLog()
-// #7 - End timer and log total time
+// #7 - End timer and log total time - //
 console.timeEnd()
 
 console.time()
 console.timeLog()
 console.timeEnd()
 
-// #8 - Only log if condition fails
+// #8 - Only log if condition fails - //
 console.assert(person.pets.find((p) => p === 'Bob'), 'Person does not have a pet named Bob')
 
-// #8 - Add CSS
+// #8 - Add CSS - //
 console.log('%c I am a logging master', 'font-weight: bold; background: red; padding: 15px')
 
 // #10 -
