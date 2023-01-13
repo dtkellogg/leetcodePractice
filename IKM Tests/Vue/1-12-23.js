9a7fb3d6ebf966d76738559a0563e6412ac5885f
Toshi <dtkellogg10@gmail.com>
2023-01-13 17:36:21 -0300

    IKM Tests - made a new directory in the root of the project to keep track and test out questions

---
+++ IKM Tests/Vue/1-12-23.js
@@ -0,0 +1,67 @@
+// ---------- # 8 ---------- //
+console.log('// ---------- #8 ---------- //')
+
+// console.log(calc(2,2))
+// var calc = (n1,n2) => {
+//   return n1 + n2
+// }
+
+// var calc = (n1,n2) => n1 + n2
+// console.log(calc(2,2))
+
+// var calc = (n1, n2) => {
+//   return n1 + n2
+// }
+// console.log(calc(2,2))
+
+var calc = ((n1,n2) => {
+  var calc = (n1 + n2)
+  return calc
+})(2,2)
+
+console.log(calc)
+
+// ---------- # 17 ---------- //
+console.log('\n\n// ---------- #17 ---------- //')
+var func1 = () => return {prop1: '1' + '1'}
+var obj1 = func1()
+console.log(obj1)
+
+
+// ---------- # 24 ---------- //
+console.log('\n\n// ---------- #24 ---------- //')
+
+var data = [0,3,0]
+// var data = [[1,2,3,[4,[5,6,14]]], [8,[9,18,[11,[12]],13]]]  // 18
+
+function test(data,ele) {
+  console.log('ele: ', ele)
+  if(typeof data == "number") {
+    return (!ele || data > ele) ? data : ele
+  } else {
+    for (var i = 0; i < data.length; i++) {
+      ele = test(data[i], ele)
+    }
+    return ele
+  }
+}
+var result = test(data)
+console.log('result: ', result)
+console.log('data.length: ', data.length)
+
+
+// ---------- #31 ---------- //
+console.log('\n\n// ---------- #31 ---------- //')
+
+// var a = [99,87,3,9,928374]
+var a = [99,87,,9,928374]
+
+a.find(function(value, index) {
+  console.log('value: ', value)
+  console.log('index: ', index)
+  if(index == 2) {
+    delete a[2]
+  }
+  console.log('a', a)
+  console.log(index)
+})
