a4b3ce168ea5d307f07f1f48ca0e2e4fa95b00db
Toshi <dtkellogg10@gmail.com>
2023-07-08 10:57:16 -0400

    Linked Lists - added another problem suggested by ChatGTP

---
+++ JS/techniques/linked list/01ChatGTP.js
@@ -0,0 +1,45 @@
+// Problem:
+// Given a linked list, write a function that reverses the order of the nodes. 
+// The function should return the new head of the reversed list.
+
+// For example, given the linked list: 1 -> 2 -> 3 -> 4 -> 5, the function 
+// should return the linked list: 5 -> 4 -> 3 -> 2 -> 1.
+
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+function reverseLinkedList(head) {
+  let prev = null;
+  let current = head;
+
+  while (current !== null) {
+    const next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
+  }
+
+  return prev;
+}
+
+// Test the function
+// Create the linked list: 1 -> 2 -> 3 -> 4 -> 5
+const head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3);
+head.next.next.next = new Node(4);
+head.next.next.next.next = new Node(5);
+
+const reversedHead = reverseLinkedList(head);
+
+// Traverse the reversed linked list and print the values
+let current = reversedHead;
+while (current !== null) {
+  console.log(current.value);
+  current = current.next;
+}
