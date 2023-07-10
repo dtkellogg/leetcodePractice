e82d8e91252d5b2c8db99f2fe24995e33a8cec24
Toshi <dtkellogg10@gmail.com>
2023-07-10 11:17:46 -0400

    Refactoring

--- JS/techniques/linked list/__linkedListBasics__.js
+++ JS/techniques/linked list/__linkedListBasics__.js
@@ -1,175 +1,8 @@
 // ------------- Definitions --------------- //
-// class ListNode {
-//   constructor(data) {
-//     this.data = data
-//     this.next = null                
-//   }
-// }
-// class LinkedList {
-//   constructor(head = null) {
-//     this.head = head
-//   }
-// }
-
-
-// // ------------- Methods --------------- //
-// LinkedList.prototype.size = function() {
-//   let count = 0; 
-//   let node = this.head;
-//   while (node) {
-//     count++;
-//     node = node.next
-//   }
-//   return count;
-// }
-
-// LinkedList.prototype.clear = function() {
-//   this.head = null;
-// }
-
-// LinkedList.prototype.getFirst = function() {
-//   return this.head;
-// }
-
-// LinkedList.prototype.getLast = function() {
-//   let lastNode = this.head;
-//   if (lastNode) {
-//     while (lastNode.next) {
-//       lastNode = lastNode.next
-//     }
-//   }
-//   return lastNode
-// }
-
-
-// // ------------- Methods to INSERT into a Singly Linked List --------------- //
-// LinkedList.prototype.insertAtBeginning = function(data) {
-// //  A newNode object is created with property data and next = null  //
-//   let newNode = new ListNode(data);
-// //  The pointer next is assigned head pointer so that both pointers now point at the same node.  //
-//   newNode.next = this.head;
-// //  As we are inserting at the beginning the head pointer needs to now point at the newNode.   //
-//   this.head = newNode;
-//   return this.head;
-// }
-
-
-// LinkedList.prototype.insertAtEnd = function(data) {
-// //  A newNode object is created with property data and next=null  //
-//   let newNode = new ListNode(data);
-// //  When head = null i.e. the list is empty, then head itself will point to the newNode.  //
-//   if(!this.head) {
-//     this.head = newNode;
-//     return this.head;
-//   }
-// //  Else, traverse the list to find the tail (the tail node will initially be pointing at null), and update the tail's next pointer.  //
-//   let tail = this.head;
-//   while(tail.next !== null) {
-//     tail = tail.next;
-//   }
-//   tail.next = newNode;
-//   return this.head;
-// }
-
-
-// //  A helper function getAt() is defined to get to the desired position. This function can also be later used for performing delete operation from a given position.  //
-
-// LinkedList.prototype.getAt = function(index) {
-//   let counter = 0;
-//   let node = this.head;
-//   while (node) {
-//     if (counter === index) {
-//       return node;
-//     }
-//     counter++;
-//     node = node.next;
-//   }
-//   return null;
-// }
-
-// //  The insertAt() function contains the steps to insert a node at a given index.  //
-
-// LinkedList.prototype.insertAt = function(data, index) {
-//   // if the list is empty i.e. head = null
-//   if (!this.head) {
-//     this.head = new ListNode(data);
-//     return;
-//   }
-//   //  if new node needs to be inserted at the front of the list i.e. before the head.   //
-//   if (index === 0) {
-//     this.head = new ListNode(data, this.head);
-//     return;
-//   }
-//   //  else, use getAt() to find the previous node.  //
-//   const previous = this.getAt(index - 1);
-//   let newNode = new ListNode(data);
-//   newNode.next = previous.next;
-//   previous.next = newNode;       
-
-//   return this.head
-// }
-
-// // ------------- Methods to DELETE from a Singly Linked List ------------- //
-
-// LinkedList.prototype.deleteFirstNode = function() {
-//   if(!this.head) {
-//     return;
-//   }
-//   this.head = this.head.next;
-//   return this.head;
-// }
-
-// LinkedList.prototype.deleteLastNode = function() {
-//   if(!this.head) {
-//     return null;
-//   }
-//   // if only one node in the list //
-//   if(!this.head.next) {
-//     this.head = null;
-//     return;
-//   }
-//   let previous = this.head;
-//   let tail = this.head.next;
-  
-//   while(tail.next !== null) {
-//     previous = tail;
-//     tail = tail.next;
-//   }
-  
-//   previous.next = null;
-//   return this.head;
-// }
-
-// LinkedList.prototype.deleteAt = function(index) {
-// // when list is empty i.e. head = null //
-//   if (!this.head) {
-//     this.head = new ListNode(data);
-//     return;
-//   }
-// // node needs to be deleted from the front of the list i.e. before the head. //
-//   if (index === 0) {
-//     this.head = this.head.next;
-//     return;
-//   }
-// // else, use getAt() to find the previous node. //
-//   const previous = this.getAt(index - 1);
-  
-//   if (!previous || !previous.next) {
-//     return;
-//   }
-  
-//   previous.next = previous.next.next;     
-//   return this.head
-// }
-
-
-////////////////////////////////////////////////////////////////////////////////
-// PRACTICE
-
 class ListNode {
   constructor(data) {
-    this.data = data;
-    this.next = null
+    this.data = data
+    this.next = null                
   }
 }
 
@@ -179,83 +12,251 @@ class LinkedList {
   }
 }
 
+
+// ------------- Methods --------------- //
 LinkedList.prototype.size = function() {
-  let count = 0, node = this.head
-  while(node) {
-    count++
+  let count = 0; 
+  let node = this.head;
+  while (node) {
+    count++;
     node = node.next
   }
-  return count
+  return count;
 }
 
 LinkedList.prototype.clear = function() {
-  this.head = null
+  this.head = null;
 }
 
-LinkedList.prototype.getFirst = function () {
-  return this.head
+LinkedList.prototype.getFirst = function() {
+  return this.head;
 }
 
-LinkedList.prototype.getLast = function () {
-  let lastNode = this.head
-  while(lastNode.next) {
-    lastNode = lastNode.next
+LinkedList.prototype.getLast = function() {
+  let lastNode = this.head;
+  if (lastNode) {
+    while (lastNode.next) {
+      lastNode = lastNode.next
+    }
   }
   return lastNode
 }
 
-LinkedList.prototype.insertAtBeginning = (data) => {
-  let newNode = new ListNode(data)
-  newNode.next = this.head
-  this.head = newNode
-  return this.head
+
+// ------------- Methods to INSERT into a Singly Linked List --------------- //
+LinkedList.prototype.insertAtBeginning = function(data) {
+//  A newNode object is created with property data and next = null  //
+  let newNode = new ListNode(data);
+//  The pointer next is assigned head pointer so that both pointers now point at the same node.  //
+  newNode.next = this.head;
+//  As we are inserting at the beginning the head pointer needs to now point at the newNode.   //
+  this.head = newNode;
+  return this.head;
 }
+
 
 LinkedList.prototype.insertAtEnd = function(data) {
-  let newNode = new ListNode(data), lastNode = this.head
-  while(lastNode.next) {
-    lastNode = lastNode.next
+//  A newNode object is created with property data and next=null  //
+  let newNode = new ListNode(data);
+//  When head = null i.e. the list is empty, then head itself will point to the newNode.  //
+  if(!this.head) {
+    this.head = newNode;
+    return this.head;
   }
-  lastNode.next = newNode
-  return this.head
+//  Else, traverse the list to find the tail (the tail node will initially be pointing at null), and update the tail's next pointer.  //
+  let tail = this.head;
+  while(tail.next !== null) {
+    tail = tail.next;
+  }
+  tail.next = newNode;
+  return this.head;
 }
+
+
+//  A helper function getAt() is defined to get to the desired position. This function can also be later used for performing delete operation from a given position.  //
 
 LinkedList.prototype.getAt = function(index) {
-  let node = this.head, count = 0
-  while(count < index) {
-    count++
-    node = node.next
+  let counter = 0;
+  let node = this.head;
+  while (node) {
+    if (counter === index) {
+      return node;
+    }
+    counter++;
+    node = node.next;
   }
-  return node
+  return null;
 }
+
+//  The insertAt() function contains the steps to insert a node at a given index.  //
 
 LinkedList.prototype.insertAt = function(data, index) {
-  let prevNode = this.getAt(index - 1), newNode = new ListNode(data)
-  newNode.next = prevNode.next
-  prevNode.next = newNode
+  // if the list is empty i.e. head = null
+  if (!this.head) {
+    this.head = new ListNode(data);
+    return;
+  }
+  //  if new node needs to be inserted at the front of the list i.e. before the head.   //
+  if (index === 0) {
+    this.head = new ListNode(data, this.head);
+    return;
+  }
+  //  else, use getAt() to find the previous node.  //
+  const previous = this.getAt(index - 1);
+  let newNode = new ListNode(data);
+  newNode.next = previous.next;
+  previous.next = newNode;       
+
   return this.head
 }
 
+// ------------- Methods to DELETE from a Singly Linked List ------------- //
+
 LinkedList.prototype.deleteFirstNode = function() {
-  this.head = this.head.next
-  return this.head
+  if(!this.head) {
+    return;
+  }
+  this.head = this.head.next;
+  return this.head;
 }
 
 LinkedList.prototype.deleteLastNode = function() {
-  let secondLastNode = this.head, lastNode = this.head.next
-  while(secondLastNode.next) {
-    secondLastNode = lastNode
-    lastNode = lastNode.next
+  if(!this.head) {
+    return null;
   }
-  secondLastNode.next = null
-  return this.head
+  // if only one node in the list //
+  if(!this.head.next) {
+    this.head = null;
+    return;
+  }
+  let previous = this.head;
+  let tail = this.head.next;
+  
+  while(tail.next !== null) {
+    previous = tail;
+    tail = tail.next;
+  }
+  
+  previous.next = null;
+  return this.head;
 }
 
 LinkedList.prototype.deleteAt = function(index) {
-  let node = this.getAt(index - 1)
-  node.next = node.next.next
+// when list is empty i.e. head = null //
+  if (!this.head) {
+    this.head = new ListNode(data);
+    return;
+  }
+// node needs to be deleted from the front of the list i.e. before the head. //
+  if (index === 0) {
+    this.head = this.head.next;
+    return;
+  }
+// else, use getAt() to find the previous node. //
+  const previous = this.getAt(index - 1);
+  
+  if (!previous || !previous.next) {
+    return;
+  }
+  
+  previous.next = previous.next.next;     
   return this.head
 }
+
+
+////////////////////////////////////////////////////////////////////////////////
+// PRACTICE
+
+// class ListNode {
+//   constructor(data) {
+//     this.data = data;
+//     this.next = null
+//   }
+// }
+
+// class LinkedList {
+//   constructor(head = null) {
+//     this.head = head
+//   }
+// }
+
+// LinkedList.prototype.size = function() {
+//   let count = 0, node = this.head
+//   while(node) {
+//     count++
+//     node = node.next
+//   }
+//   return count
+// }
+
+// LinkedList.prototype.clear = function() {
+//   this.head = null
+// }
+
+// LinkedList.prototype.getFirst = function () {
+//   return this.head
+// }
+
+// LinkedList.prototype.getLast = function () {
+//   let lastNode = this.head
+//   while(lastNode.next) {
+//     lastNode = lastNode.next
+//   }
+//   return lastNode
+// }
+
+// LinkedList.prototype.insertAtBeginning = (data) => {
+//   let newNode = new ListNode(data)
+//   newNode.next = this.head
+//   this.head = newNode
+//   return this.head
+// }
+
+// LinkedList.prototype.insertAtEnd = function(data) {
+//   let newNode = new ListNode(data), lastNode = this.head
+//   while(lastNode.next) {
+//     lastNode = lastNode.next
+//   }
+//   lastNode.next = newNode
+//   return this.head
+// }
+
+// LinkedList.prototype.getAt = function(index) {
+//   let node = this.head, count = 0
+//   while(count < index) {
+//     count++
+//     node = node.next
+//   }
+//   return node
+// }
+
+// LinkedList.prototype.insertAt = function(data, index) {
+//   let prevNode = this.getAt(index - 1), newNode = new ListNode(data)
+//   newNode.next = prevNode.next
+//   prevNode.next = newNode
+//   return this.head
+// }
+
+// LinkedList.prototype.deleteFirstNode = function() {
+//   this.head = this.head.next
+//   return this.head
+// }
+
+// LinkedList.prototype.deleteLastNode = function() {
+//   let secondLastNode = this.head, lastNode = this.head.next
+//   while(secondLastNode.next) {
+//     secondLastNode = lastNode
+//     lastNode = lastNode.next
+//   }
+//   secondLastNode.next = null
+//   return this.head
+// }
+
+// LinkedList.prototype.deleteAt = function(index) {
+//   let node = this.getAt(index - 1)
+//   node.next = node.next.next
+//   return this.head
+// }
 
 
 ////////////////////////////////////////////////////////////////////////////////
--- JS/techniques/recursion/50pow(x,n).js
+++ JS/techniques/recursion/50pow(x,n).js
@@ -18,15 +18,13 @@ var myPow = function(x, n) {
 ////////////////////////////////////////////////////////////////////////////////
 // 🔥🔥🔥 same as above, but w/o comments
 
-// var myPow = function(x, n) {
-//     if (n===0) return 1;
-  
-//     let pow = Math.abs(n);
-    
-// 	let result = pow%2===0 ? myPow(x*x,pow/2) : myPow(x*x,(pow-1)/2) * x;
+var myPow = function(x, n) {
+  if (n===0) return 1;
+  let pow = Math.abs(n);
+	let result = pow%2===0 ? myPow(x*x,pow/2) : myPow(x*x,(pow-1)/2) * x;
 
-//     return n < 0 ? 1/result : result;
-// };
+  return n < 0 ? 1/result : result;
+};
 
 
 ////////////////////////////////////////////////////////////////////////
