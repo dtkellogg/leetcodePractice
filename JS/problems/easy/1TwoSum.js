325cf5c9a4de7a6d51e351c48f6da1370642b618
Toshi <dtkellogg10@gmail.com>
2023-07-11 14:05:15 -0400

    Easy problems review

--- JS/problems/easy/1TwoSum.js
+++ JS/problems/easy/1TwoSum.js
@@ -29,6 +29,15 @@ nums = [2,7,11,15], target = 17
 // PRACTICE
 
 var twoSum = function(nums, target) {
+  let map = new Map();
+  for(let i = 0; i < nums.length; i ++) {
+      if(map.has(target - nums[i])) {
+          return [map.get(target - nums[i]), i];
+      } else {
+          map.set(nums[i], i);
+      }
+  }
+	return [];
 };
 
 console.log(twoSum(nums, target))
