dbf0cad29934abc0f735ffce0420672d04e72a57
Toshi <dtkellogg10@gmail.com>
2023-07-12 15:44:34 -0400

    Refactoring

--- JS/problems/easy/383ransomNote.js
+++ JS/problems/easy/383ransomNote.js
@@ -2,14 +2,14 @@ ransomNote = "a", magazine = "b"
 ransomNote = "aa", magazine = "aab"
 
 var canConstruct = function(ransomNote, magazine) {
-    if ( ransomNote.length > magazine.length ) return false;
-    const oldMagazineLength = magazine.length;
-    ransomNote.split('').forEach(item => {
-        magazine = magazine.replace(item, '');
-    });
+  if ( ransomNote.length > magazine.length ) return false;
+  const oldMagazineLength = magazine.length;
+  ransomNote.split('').forEach(item => {
+    magazine = magazine.replace(item, '');
+  });
 
-    console.log(`ransomNote: ${ransomNote}, oldMagazineLength: ${oldMagazineLength}, magazine.length: ${magazine.length}`)
-    return oldMagazineLength === magazine.length + ransomNote.length;
+  console.log(`ransomNote: ${ransomNote}, oldMagazineLength: ${oldMagazineLength}, magazine.length: ${magazine.length}`)
+  return oldMagazineLength === magazine.length + ransomNote.length;
 };
 
 
