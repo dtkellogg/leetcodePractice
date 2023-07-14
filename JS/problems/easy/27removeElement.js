3c4d598fbaa93d1c399192333a1306e7eca16bd8
Toshi <dtkellogg10@gmail.com>
2023-07-14 16:41:37 -0400

    Easy problems - cleaning up code

--- JS/problems/easy/27removeElement.js
+++ JS/problems/easy/27removeElement.js
@@ -2,15 +2,15 @@ nums = [0,1,2,2,3,0,4,2], val = 2
 
 var removeElement = function(nums, val) {
   let count = 0
-    while (nums.includes(val)) {
-        var index = nums.indexOf(val);
-        if (index !== -1) {
-            nums.splice(index, 1)
-            count++
-        }
+  while (nums.includes(val)) {
+      var index = nums.indexOf(val);
+      if (index !== -1) {
+        nums.splice(index, 1)
+        count++
       }
-      console.log(count)
-      return nums
+    }
+    console.log(count)
+    return nums
 };
 
 
--- JS/problems/easy/383ransomNote.js
+++ JS/problems/easy/383ransomNote.js
@@ -2,13 +2,13 @@ ransomNote = "a", magazine = "b"
 ransomNote = "aa", magazine = "aab"
 
 var canConstruct = function(ransomNote, magazine) {
-  if ( ransomNote.length > magazine.length ) return false;
+  if (ransomNote.length > magazine.length) return false;
   const oldMagazineLength = magazine.length;
   ransomNote.split('').forEach(item => {
     magazine = magazine.replace(item, '');
   });
 
-  console.log(`ransomNote: ${ransomNote}, oldMagazineLength: ${oldMagazineLength}, magazine.length: ${magazine.length}`)
+  console.log(`ransomNote.length: ${ransomNote.length}, oldMagazineLength: ${oldMagazineLength}, magazine.length: ${magazine.length}`)
   return oldMagazineLength === magazine.length + ransomNote.length;
 };
 
