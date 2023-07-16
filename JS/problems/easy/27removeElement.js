920fb57ff77f5b1b817db2b591c3bdc745a32223
Toshi <dtkellogg10@gmail.com>
2023-07-16 19:25:31 -0400

    Refactoring

--- JS/problems/easy/27removeElement.js
+++ JS/problems/easy/27removeElement.js
@@ -3,14 +3,14 @@ nums = [0,1,2,2,3,0,4,2], val = 2
 var removeElement = function(nums, val) {
   let count = 0
   while (nums.includes(val)) {
-      var index = nums.indexOf(val);
-      if (index !== -1) {
-        nums.splice(index, 1)
-        count++
-      }
+    var index = nums.indexOf(val);
+    if (index !== -1) {
+      nums.splice(index, 1)
+      count++
     }
-    console.log(count)
-    return nums
+  }
+  console.log(count)
+  return nums
 };
 
 
