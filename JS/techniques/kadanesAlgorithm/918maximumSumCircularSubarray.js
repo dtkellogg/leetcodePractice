dbeb79d3a55c16f3835b1116039702e22048b55f
Toshi <dtkellogg10@gmail.com>
2023-07-20 14:24:49 -0400

    Techniques - added the Kadanes Algorithm with 2 problems

---
+++ JS/techniques/kadanesAlgorithm/918maximumSumCircularSubarray.js
@@ -0,0 +1,53 @@
+nums = [1,-2,3,-2]  // 3
+nums = [5,-3,5]  // 10
+nums = [-3,-2,-3]  // -2
+
+function maxSubarraySumCircular(nums) {
+  // Check if all elements are negative (corner case)
+  let allNegative = true;
+  for (let num of nums) {
+    if (num >= 0) {
+      allNegative = false;
+      break;
+    }
+  }
+
+  // If all elements are negative, return the maximum element as the result
+  if (allNegative) {
+    let maxElement = Math.max(...nums);
+    return maxElement;
+  }
+
+  // Case 1: Maximum subarray is non-circular (same as regular Kadane's Algorithm)
+  let maxSumNonCircular = kadanesAlgorithm(nums);
+
+  // Case 2: Maximum subarray is circular
+  let totalSum = 0;
+  for (let num of nums) {
+    totalSum += num;
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    nums[i] = -nums[i];
+  }
+
+  let minSum = kadanesAlgorithm(nums);
+
+  let maxSumCircular = totalSum + minSum;
+
+  return Math.max(maxSumNonCircular, maxSumCircular);
+}
+
+function kadanesAlgorithm(arr) {
+  let currentMax = arr[0];
+  let globalMax = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    currentMax = Math.max(arr[i], currentMax + arr[i]);
+    globalMax = Math.max(globalMax, currentMax);
+  }
+
+  return globalMax;
+}
+
+console.log(maxSubarraySumCircular(nums))
