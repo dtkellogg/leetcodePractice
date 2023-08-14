df5f2ca6d3229706f68ad43f865b8adab1c8dab1
Toshi <dtkellogg10@gmail.com>
2023-08-14 11:44:17 -0400

    Refactoring - standardizing tab sizing to 4 two spaces

--- JS/problems/easy/557reverseWordsInAString.js
+++ JS/problems/easy/557reverseWordsInAString.js
@@ -2,45 +2,42 @@ s = "Let's take LeetCode contest"
 
 // 🔥🔥🔥
 var reverseWords = function(s) {
-    let res = '';
-    let word = '';
-    for (let c of s) {
-        console.log(`c: ${c}, word: ${word}, res: ${res}`);
-        if (c === ' ') {
-            res += word + c;
-            word = '';
-        } else {
-            word = c + word;
-        }
-        console.log(object)
+  let res = '';
+  let word = '';
+  for (let c of s) {
+    console.log(`c: ${c}, word: ${word}, res: ${res}`);
+    if (c === ' ') {
+      res += word + c;
+      word = '';
+    } else {
+      word = c + word;
     }
-    return res + word;
+    console.log(object)
+  }
+  return res + word;
 };
-
-
-
 
 
 ////////////////////////////////////////////////////////////////
 // the function directly below is 84ms (faster than 91.5%)
 // var reverseWords = function(s) {
-//     const desired = []
-//     const words = s.split(" ")
-    
-//     words.map((word) => {
-//         desired.push(word.split("").reverse().join(""))
-//     })
-    
-//     return desired.join(" ")
+//   const desired = []
+//   const words = s.split(" ")
+  
+//   words.map((word) => {
+//     desired.push(word.split("").reverse().join(""))
+//   })
+  
+//   return desired.join(" ")
 // };
 
 
-//  mine:
+// mine:
 // var reverseWords = function(s) {
-//     return s.split(" ").map((word) => {
-//         const chars = word.split("")
-//         return chars.reverse().join("")
-//     }).join(" ")
+//   return s.split(" ").map((word) => {
+//     const chars = word.split("")
+//     return chars.reverse().join("")
+//   }).join(" ")
 // };
 
 
