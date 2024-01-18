12dc2b728d1a8ebce5d65f565477f955dbe9ef39
Toshi <dtkellogg10@gmail.com>
2024-01-18 14:21:03 -0800

    Refactoring

--- Typing/1.js
+++ Typing/1.js
@@ -14,19 +14,90 @@ const maxAreaOfIsland = (grid) => {}
 
 // ---------------------------------------------------------------- //
 
-
+// 1.
 var hello = function(nums, target) {
   return 837
 }
 
+// 2.
 function lengthOfLongestSubstring(s) {
   return 65
 }
 
+// 3.
 const maxAreaOfIsland = (grid) => {}
 
-
+// 1.
 var hello = function(nums, target) {
   return 837
 }
+
+// 2.
+function lengthOfLongestSubstring(s) {
+  return 65
+}
+
+// 3.
+const maxAreaOfIsland = (grid) => {}
+
+// 1.
+var hello = function(nums, target) {
+  return 837
+}
+
+// 2.
+function lengthOfLongestSubstring(s) {
+  return 65
+}
+
+// 3.
+const maxAreaOfIsland = (grid) => {}
+
+
+// ---------------------------------------------------------------- //
+
+
+// 1.
+45 + 36 = 81
+95 - 40 = 55
+5 * 60 = 300
+$81 / 3 = $27
+6 % 4 = 2
+2 ^ 3 = 8
+if(13 >= x) return res
+return x + 3 >= 9 ? 'nice!' : 'try again'
+
+
+// ---------------------------------------------------------------- //
+
+// 1.
+45 + 36 = 81
+95 - 40 = 55
+5 * 60 = 300
+$81 / 3 = $27
+6 % 4 = 2
+2 ^ 3 = 8
+if(13 >= x) return res
+return x + 3 >= 9 ? 'nice' : 'try again'
+
+// 2.
+45 + 36 = 81
+95 - 40 = 55
+5 * 60 = 300
+$81 / 3 = $27
+6 % 4 = 2
+2 ^ 3 = 8
+if(13 >= x) return res
+return x + 3 >= 9 ? 'nice' : 'try again'
+
+// 3.
+45 + 36 = 81
+95 - 40 = 55
+$81 / 3 = $27
+6 % 4 = 2
+2 ^ 3 = 8
+if(13 >= x) return res
+return x + 3 >= 9 ? 'nice' : 'try again'
+
+// ---------------------------------------------------------------- //
 
