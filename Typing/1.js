55cf05c2ba7c4954781d1b45d331125ba5ac4663
Toshi <dtkellogg10@gmail.com>
2024-01-19 10:40:20 -0800

    Adding more typing practice

--- Typing/1.js
+++ Typing/1.js
@@ -9,49 +9,13 @@ function lengthOfLongestSubstring(s) {
 };
 
 // 3.
-const maxAreaOfIsland = (grid) => {}
+const maxAreaOfIsland = (grid) => {
+  return [...res, 1, 3]
+}
 
 
 // ---------------------------------------------------------------- //
 
-// 1.
-var hello = function(nums, target) {
-  return 837
-}
-
-// 2.
-function lengthOfLongestSubstring(s) {
-  return 65
-}
-
-// 3.
-const maxAreaOfIsland = (grid) => {}
-
-// 1.
-var hello = function(nums, target) {
-  return 837
-}
-
-// 2.
-function lengthOfLongestSubstring(s) {
-  return 65
-}
-
-// 3.
-const maxAreaOfIsland = (grid) => {}
-
-// 1.
-var hello = function(nums, target) {
-  return 837
-}
-
-// 2.
-function lengthOfLongestSubstring(s) {
-  return 65
-}
-
-// 3.
-const maxAreaOfIsland = (grid) => {}
 
 
 // ---------------------------------------------------------------- //
@@ -64,40 +28,13 @@ const maxAreaOfIsland = (grid) => {}
 $81 / 3 = $27
 6 % 4 = 2
 2 ^ 3 = 8
-if(13 >= x) return res
+if(13 >= x && (x !== 69)) return res
 return x + 3 >= 9 ? 'nice!' : 'try again'
 
 
 // ---------------------------------------------------------------- //
 
-// 1.
-45 + 36 = 81
-95 - 40 = 55
-5 * 60 = 300
-$81 / 3 = $27
-6 % 4 = 2
-2 ^ 3 = 8
-if(13 >= x) return res
-return x + 3 >= 9 ? 'nice' : 'try again'
 
-// 2.
-45 + 36 = 81
-95 - 40 = 55
-5 * 60 = 300
-$81 / 3 = $27
-6 % 4 = 2
-2 ^ 3 = 8
-if(13 >= x) return res
-return x + 3 >= 9 ? 'nice' : 'try again'
-
-// 3.
-45 + 36 = 81
-95 - 40 = 55
-$81 / 3 = $27
-6 % 4 = 2
-2 ^ 3 = 8
-if(13 >= x) return res
-return x + 3 >= 9 ? 'nice' : 'try again'
 
 // ---------------------------------------------------------------- //
 
